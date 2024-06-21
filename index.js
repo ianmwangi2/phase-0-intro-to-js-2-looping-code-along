5ab736c27c86fd501745f56163350bcc8993e435
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-06-21 16:27:19 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+let names = ['Quiin', 'Nelly', 'Clark'];
+  let eventName = 'birthday';
+function writeCards(names, eventName) {
+    let cards = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      cards.push(message);
+    }
+  
+    return cards;
+  }
+
+  countDown(10);
+  function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
+
